7051f2ffaac7ddccfb361ecd69772b93ff3e2389
Syart Veliu <veliusyart@gmail.com>
2024-11-21 01:41:53 +0100

    Maximum of 3 tries
    Maximum of 3 tries

--- sessions/final-project-omer/main.js
+++ sessions/final-project-omer/main.js
@@ -13,43 +13,77 @@ let allData = [
     }
 ];
 
-let objMovie = {};
-objMovie.year = "2030";
-objMovie.name = "Shqiptaret e pare ne Hone";
-objMovie.thumb = "the=taste-of-things.jpeg";
-objMovie.reviews = "444";
-
-allData.push(objMovie);
-
 const allMovies = document.querySelector("#allMovies");
 const addBTN = document.querySelector("#addBTN");
 const movieModal = document.querySelector("#movieModal");
 const cancelBTN = document.querySelector("#cancelBTN");
+const submitBTN = document.querySelector("#submitBTN");
+const year = document.querySelector("#year");
+const movieTitle = document.querySelector("#movieTitle");
+const thumb = document.querySelector("#thumb");
+const reviews = document.querySelector("#reviews");
 
-let recordsHTML = '';
-for(let i=0; i<allData.length; i++){
-    recordsHTML += "<tr>";
-    recordsHTML +=`
+let editAction = false;
+let currentMovie = 0;
+
+listTable();
+function listTable() {
+    let recordsHTML = '';
+    for (let i = 0; i < allData.length; i++) {
+        recordsHTML += "<tr>";
+        recordsHTML += `
                 <td>${allData[i].year}</td>
                 <td>${allData[i].name}</td>
                 <td><img width="50" src="${allData[i].thumb}" alt=""></td>
                 <td>${allData[i].reviews}</td>
                 <td>
-                    <select name="" id="">
-                        <option value="">Edit</option>
-                        <option value="">Remove</option>
-                    </select>
+                    <button onclick="editMovie(${i})">Edit</button> 
+                    <button onclick="removeMovie(${i})">Remove</button>
                 </td>
     `;
-    recordsHTML += "</tr>";
+        recordsHTML += "</tr>";
+    }
+
+    allMovies.innerHTML = recordsHTML;
 }
 
-allMovies.innerHTML = recordsHTML;
-
-addBTN.addEventListener("click", ()=>{
+addBTN.addEventListener("click", () => {
     movieModal.style.display = "block";
 });
 
-cancelBTN.addEventListener("click", ()=>{
+cancelBTN.addEventListener("click", removeModal);
+
+submitBTN.addEventListener('click', submitForm);
+
+function submitForm(){
+    if(!editAction){
+        let objMovie = {};
+        objMovie.year = year.value;
+        objMovie.name = movieTitle.value;
+        objMovie.thumb = thumb.value;
+        objMovie.reviews = reviews.value;
+
+        allData.push(objMovie);
+    } else {
+        movieModal.style.display = "block";
+        year.value = allData[currentMovie].year;
+        movieTitle.value = allData[currentMovie].name;
+        reviews.value = allData[currentMovie].reviews;
+    }
+    
+    listTable();
+    removeModal();
+}
+
+function removeModal(){
+    year.value = "";
+    movieTitle.value = "";
+    reviews.value = "";
     movieModal.style.display = "none";
-});
+}
+
+function editMovie(m){
+    currentMovie = m;
+    editAction = true;
+    submitForm();
+}
